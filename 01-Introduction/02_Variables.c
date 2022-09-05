2f2747f4fe8498ecdfe590af3ae0e8de3965359a
DipadityaDas <DipadityaDas@gmail.com>
2022-09-05 23:06:34 +0530

    Variables

---
+++ 01-Introduction/02_Variables.c
@@ -0,0 +1,25 @@
+# include<stdio.h>
+
+int main()
+{
+    // Variables are name of a memory location which store data 
+    // which can be changed during runtime.
+
+    int number = 25; // Integer Data Type
+    char star = "*"; // Character Data Type
+    float pi = 3.14; // Floating point Data Type
+
+    int age = 25;   
+    age = 60;        // Value of the variable can be changed within a program
+
+    int Number = 100; // Variables are case-sensitive
+
+    char _fail = "#"; // 1st Character is Alphabet or '_'
+    char _pass = "$"; 
+
+    long electricity_bill = 280; // No Blank space or comma between a single variable.
+
+    // C language do not have boolean or string.
+
+    return 0;
+}
