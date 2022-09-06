327d40d4dd91762553b9f2718e0090a1f48d6d24
DipadityaDas <DipadityaDas@gmail.com>
2022-09-06 20:32:56 +0530

    Program Structure

---
+++ 01-Introduction/05_ProgramStructure.c
@@ -0,0 +1,42 @@
+// Referring to the First Program
+// The First line is basically a Preprocessor Directive
+// which tells the complier to include information about the 
+// standard input/output library. This line appears at the 
+// beginning of many C sources files.
+
+// After that we have defined a function named main
+// that receives no argument values which means there are no 
+// values in between parentheses.
+
+// The statements of main are enclosed in braces,
+// which is also called function block.
+
+// A function is called by naming it, followed by a parenthesized 
+// list of arguements.
+
+// main calls library function printf to print the argument 
+// "Hello, World\n" which is a sequence of characters.
+
+// The Sequence \n in the String is C notation for the newline 
+// character, which when printed advances the output to the left 
+// margins on the next line. If we leave out the \n, we will find
+// that there is no line advance after the output is printed.
+// We must use \n to print newline in printf.
+
+// \n represents a single character.
+// An Escape Sequence like \n provides a general and extensible
+// mechanism for representing hart-to-type or invisible characters.
+// Among the others that C provides are \t for tab, \b for backspace,
+// \" for Double Quotes and \\ for backslash itself. 
+
+
+
+
+
+
+
+
+
+
+
+
