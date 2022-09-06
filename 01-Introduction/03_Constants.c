ef1da5b7019403dc6a99fb2ad9cf42cdd03a0f4e
DipadityaDas <DipadityaDas@gmail.com>
2022-09-06 19:28:30 +0530

    Constants

---
+++ 01-Introduction/03_Constants.c
@@ -0,0 +1,16 @@
+# include <stdio.h>
+
+int main()
+{
+    // Constants or Literals are values that do not change during runtime.
+    // There are three types of Constants
+    // 1. Interger Constants - 1,2,3,0,-2,-5 etc
+    // 2. Real Constants - 3.14, 1.602 etc
+    // 3. Character Constants - 'a','A' etc
+
+    int radius = 5;
+    float pi = 3.14;
+    char hashtag = "#";
+
+    return 0;
+}
