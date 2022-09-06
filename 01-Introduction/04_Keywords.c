129dc2f59033eb62662914072e2dc99ab273a1aa
DipadityaDas <DipadityaDas@gmail.com>
2022-09-06 19:37:50 +0530

    Keywords

---
+++ 01-Introduction/04_Keywords.c
@@ -0,0 +1,21 @@
+// Keywords are predefined, reserved words used in programming that have 
+// special meanings to the compiler. Keywords are part of the syntax and 
+// they cannot be used as an identifier. 
+
+// For example:
+// int money;
+
+// Here, int is a keyword that indicates money is a variable of type int (integer).
+
+// As C is a case sensitive language, all keywords must be written in lowercase. 
+// Here is a list of all keywords allowed in ANSI C.
+
+//                  C Keywords
+// auto	        double	    int	        struct
+// break	    else	    long	    switch
+// case	        enum	    register	typedef
+// char	        extern	    return	    union
+// continue	    for	        signed	    void
+// do	        if	        static	    while
+// default	    goto	    sizeof	    volatile
+// const	    float	    short	    unsigned
