9c0ebfb15074afaeb16a634804acafb6d2e3d990
DipadityaDas <DipadityaDas@gmail.com>
2022-09-20 23:29:59 +0530

    while loop

---
+++ 04_Looping_Statements/02_while_loop.c
@@ -0,0 +1,18 @@
+// C program to print all alphabets using while loop
+
+#include <stdio.h>
+
+int main()
+{
+	char ch = 'a';
+
+	printf("Alphabets from a - z are: \n");
+	
+	while (ch <= 'z')
+	{
+		printf("%c\n", ch);
+		ch++;
+	}
+
+	return 0;
+}
