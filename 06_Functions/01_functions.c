8aff6928830159c659ffbf83e2345434ce14a443
DipadityaDas <DipadityaDas@gmail.com>
2022-09-27 20:44:39 +0530

    Function in C

---
+++ 06_Functions/01_functions.c
@@ -0,0 +1,14 @@
+/****************************************************************************************************************************************
+ * Functions in C Programming Language
+ * A function is a collection of statements grouped together to do some specific task.
+ * 
+ * Why use functions ?
+ * A program is a collection of small tasks that may depend on other tasks. 
+ * You can use two approaches to write a program.
+ * 1. Write code for all tasks in single place, in the main() function sequentially.  
+ * 2. Divide code for all tasks into separate functions. Use main() function as driver block to execute all other tasks.
+ * 
+ * NOTE: Without main() function C program will not execute.
+ * 
+ *
+ */
