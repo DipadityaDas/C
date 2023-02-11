4bd690b30802fdddcfe6bd6850efb15d7316f7e5
DipadityaDas <DipadityaDas@gmail.com>
2023-02-11 23:07:25 +0530

    DSA - Temperature

---
+++ 10_DSA/10_temperature.c
@@ -0,0 +1,17 @@
+// Write a program to convert Fahrenheit to Celsius.
+
+#include <stdio.h>
+
+int main()
+{
+	float fahrenheit, celsius;
+
+	printf("Enter temperature in Fahrenheit: ");
+	scanf("%f", &fahrenheit);
+
+	celsius = (fahrenheit - 32) * 5 / 9;
+
+	printf("%.2f Fahrenheit = %.2f Celsius\n", fahrenheit, celsius);
+
+	return 0;
+}
