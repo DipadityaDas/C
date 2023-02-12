7ca57f0846e37b9aa2e370fa032529aaa907dfd0
DipadityaDas <DipadityaDas@gmail.com>
2023-02-12 23:10:56 +0530

    DSA - Palindrome Number

---
+++ 10_DSA/13_palindrome.c
@@ -0,0 +1,26 @@
+// Write a program in C to check whether a number is palindrome or not using iteration.
+#include <stdio.h>
+
+int main()
+{
+	int n, reverse = 0, temp;
+	printf("Enter the number : n");
+	scanf("%d", &n);
+	temp = n;
+	while (temp != 0)
+	{
+		reverse = reverse * 10 + temp % 10;
+		temp = temp / 10;
+	}
+
+	if (reverse == n)
+	{
+		printf("The number is a Palindrome");
+	}
+	else
+	{
+		printf("The number is not a Palindrom");
+	}
+
+	return 0;
+}
