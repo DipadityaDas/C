e8c9c9b31beef539834a5a80273bed8afc0bd02c
DipadityaDas <DipadityaDas@gmail.com>
2023-02-14 23:30:58 +0530

    DSA - LCM

---
+++ 10_DSA/18_LCM.c
@@ -0,0 +1,29 @@
+// Write a program in C to print LCM of two numbers.
+
+#include <stdio.h>
+
+int main()
+{
+	int num1, num2, maxNum;
+	printf("Program to find LCM of two numbers\n");
+
+	printf("Enter the first number: ");
+	scanf("%d", &num1);
+
+	printf("Enter the second number: ");
+	scanf("%d", &num2);
+
+	maxNum = (num1 > num2) ? num1 : num2;
+
+	while (1)
+	{
+		if (maxNum % num1 == 0 && maxNum % num2 == 0)
+		{
+			printf("The LCM of %d and %d is %d.", num1, num2, maxNum);
+			break;
+		}
+		++maxNum;
+	}
+
+	return 0;
+}
