c064a4f7310779f40ed4685e7d50ffa554b90f9f
DipadityaDas <DipadityaDas@gmail.com>
2023-02-15 23:12:17 +0530

    DSA - HCF of two numbers using Divison Method

---
+++ 10_DSA/19_HCF.c
@@ -0,0 +1,22 @@
+// Write a C Program to find HCF/GCD of two positive number using Division Method.
+
+#include <stdio.h>
+
+int main()
+{
+	int num1, num2, i, hcf;
+
+	printf("Enter two integers: ");
+	scanf("%d %d", &num1, &num2);
+
+	for (i = 1; i <= num1 && i <= num2; ++i)
+	{
+		// Checks if i is factor of both integers
+		if (num1 % i == 0 && num2 % i == 0)
+			hcf = i;
+	}
+
+	printf("G.C.D./H.C.F. of %d and %d is %d", num1, num2, hcf);
+
+	return 0;
+}
