a3614ddce5e6ca5af6774bfce6136aec22cff9e6
DipadityaDas <DipadityaDas@gmail.com>
2023-02-26 23:35:06 +0530

    DSA - Largest Number in an Array

---
+++ 10_DSA/35_Array_1.c
@@ -0,0 +1,29 @@
+// Write a C Program to print the Largest number of the User-defined Array.
+
+#include <stdio.h>
+
+int main()
+{
+	int size, i, largest;
+
+	printf("Enter the Size of the Array : ");
+	scanf("%d", &size);
+
+	int arr[size];
+
+	printf("Enter the array elements :\n");
+	for (i = 0; i < size; ++i)
+	{
+		scanf("%d", &arr[i]);
+	}
+
+	largest = arr[0];
+	for (i = 1; i < size; ++i)
+	{
+		if(largest < arr[i])
+		largest = arr[i];
+	}
+
+	printf("The Largest Number in the Array : %d", largest);
+	return 0;
+}
