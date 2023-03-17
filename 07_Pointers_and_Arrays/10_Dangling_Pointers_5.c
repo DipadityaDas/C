fdc7b7b88be7ecef9212e167e0456604fd05b8ff
DipadityaDas <DipadityaDas@gmail.com>
2023-03-17 23:13:31 +0530

    Dangling Pointer in C Part 5

--- 07_Pointers_and_Arrays/10_Dangling_Pointers_5.c
+++ 07_Pointers_and_Arrays/10_Dangling_Pointers_5.c
@@ -1,7 +1,30 @@
 /***********************************************************************************************************
  * Dangling Pointers in C - Part 5
  *
- *
+ * 2. Static variables with global scope
+ * 
+ * Static variables are the variables that remain in the memory until the execution of the program finishes. 
+ * We can use the static keyword to declare a static variable in our program.
+ * 
+ * Syntax :
+ * static int x;
+ * 
+ * x is a static integer variable with global scope and will stay in the memory until the execution of the 
+ * program finishes. Global scope means we will be able to use our variable anywhere in our program, we can 
+ * declare our variable anywhere in the program inside any function or in an inner block of code.
+ * 
+ * x is a static integer variable with global scope and will stay in the memory until the execution of the 
+ * program finishes. Global scope means we will be able to use our variable anywhere in our program, we can 
+ * declare our variable anywhere in the program inside any function or in an inner block of code.
+ * 
+ * The example shows how the static variable remains in the memory and avoids the condition of dangling pointers.
+ * 
+ *   i. A function() is called inside the main() function, memory is allocated by the system for the function() block.
+ *  ii. A global variable static temp is declared and initialized inside the function(). Let the address of temp is 
+ *      2000. After returning the address of the temp function execution finishes, temp remains in the memory 
+ *      because of its static nature.
+ * iii. Returned address 2000 is stored in ptr pointer and as temp is not deleted from the memory, ptr points to 
+ *      temp variable with value 10. So, ptr will not act as a dangling pointer.
  */
 
 #include <stdio.h>
@@ -29,3 +52,12 @@ int main()
 
 	return 0;
 }
+
+/**
+ * Explanation :
+ * 
+ * 1. notADanglingPointer() function now returns the address of a global variable.
+ * 2. ptr contains the address of temp variable i.e. a global variable.
+ * 3. ptr is acting as a Normal Pointer in this code.
+ * 4. Output will be value 10 i.e. stored in temp variable.
+*/
