4a861cf4072ca8418bf4c9997c0478906fd29e54
DipadityaDas <DipadityaDas@gmail.com>
2023-05-03 23:49:48 +0530

    Revision Done

--- 01-Introduction/02_Variables.c
+++ 01-Introduction/02_Variables.c
@@ -2,24 +2,24 @@
 
 int main()
 {
-    // Variables are name of a memory location which store data 
-    // which can be changed during runtime.
+	// Variables are name of a memory location which store data
+	// which can be changed during runtime.
 
-    int number = 25; // Integer Data Type
-    char star = "*"; // Character Data Type
-    float pi = 3.14; // Floating point Data Type
+	int number = 25; // Integer Data Type
+	char star = "*"; // Character Data Type
+	float pi = 3.14; // Floating point Data Type
 
-    int age = 25;   
-    age = 60;        // Value of the variable can be changed within a program
+	int age = 25;   
+	age = 60;        // Value of the variable can be changed within a program
 
-    int Number = 100; // Variables are case-sensitive
+	int Number = 100; // Variables are case-sensitive
 
-    char _fail = "#"; // 1st Character of a single variable is Alphabet or '_'
-    char _pass = "$"; 
+	char _fail = "#"; // 1st Character of a single variable is Alphabet or '_'
+	char _pass = "$";
 
-    long electricity_bill = 280; // No Blank space or comma between a single variable.
+	long electricity_bill = 280; // No Blank space or comma between a single variable.
 
-    // C language do not have boolean or string.
+	// C language do not have boolean or string.
 
-    return 0;
+	return 0;
 }
