e61eca67c6fcba1a5f47325036177e1a3f34ff6b
DipadityaDas <DipadityaDas@gmail.com>
2023-05-04 23:47:37 +0530

    Revision Done

--- 01-Introduction/03_Constants.c
+++ 01-Introduction/03_Constants.c
@@ -2,16 +2,16 @@
 
 int main()
 {
-    // Constants or Literals are values that do not change during runtime.
-    // There are three types of Constants
-    // 1. Interger Constants - 1,2,3,0,-2,-5 etc
-    // 2. Real Constants - 3.14, 1.602 etc
-    // 3. Character Constants - 'a','A' etc
-    // 4. String Constants - "Hello, World" etc
+	// Constants or Literals are values that do not change during runtime.
+	// There are three types of Constants
+	// 1. Interger Constants - 1,2,3,0,-2,-5 etc
+	// 2. Real Constants - 3.14, 1.602 etc
+	// 3. Character Constants - 'a','A' etc
+	// 4. String Constants - "Hello, World" etc
 
-    int radius = 5;
-    float pi = 3.14;
-    char hashtag = "#";
+	int radius = 5;
+	float pi = 3.14;
+	char hashtag = "#";
 
-    return 0;
+	return 0;
 }
