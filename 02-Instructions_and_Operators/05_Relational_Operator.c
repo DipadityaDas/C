03268431995b083497bcda66a22d3189c7a8cdf7
DipadityaDas <DipadityaDas@gmail.com>
2023-05-09 23:46:08 +0530

    Revisied Relational Operator

--- 02-Instructions_and_Operators/05_Relational_Operator.c
+++ 02-Instructions_and_Operators/05_Relational_Operator.c
@@ -8,25 +8,25 @@
 
 int main()
 {
-    int a = 5, b = 5, c = 10;
-    // Equal to (==)
-    printf("%d == %d is %d \n", a, b, a == b);
-    printf("%d == %d is %d \n", a, c, a == c);
-    // Greater than (>)
-    printf("%d > %d is %d \n", a, b, a > b);
-    printf("%d > %d is %d \n", a, c, a > c);
-    // Less than (<)
-    printf("%d < %d is %d \n", a, b, a < b);
-    printf("%d < %d is %d \n", a, c, a < c);
-    // Not equal to (!=)
-    printf("%d != %d is %d \n", a, b, a != b);
-    printf("%d != %d is %d \n", a, c, a != c);
-    // Greater than or equal to (>=)
-    printf("%d >= %d is %d \n", a, b, a >= b);
-    printf("%d >= %d is %d \n", a, c, a >= c);
-    // Less than or equal to (<=)
-    printf("%d <= %d is %d \n", a, b, a <= b);
-    printf("%d <= %d is %d \n", a, c, a <= c);
+	int a = 5, b = 5, c = 10;
+	// Equal to (==)
+	printf("%d == %d is %d \n", a, b, a == b);
+	printf("%d == %d is %d \n", a, c, a == c);
+	// Greater than (>)
+	printf("%d > %d is %d \n", a, b, a > b);
+	printf("%d > %d is %d \n", a, c, a > c);
+	// Less than (<)
+	printf("%d < %d is %d \n", a, b, a < b);
+	printf("%d < %d is %d \n", a, c, a < c);
+	// Not equal to (!=)
+	printf("%d != %d is %d \n", a, b, a != b);
+	printf("%d != %d is %d \n", a, c, a != c);
+	// Greater than or equal to (>=)
+	printf("%d >= %d is %d \n", a, b, a >= b);
+	printf("%d >= %d is %d \n", a, c, a >= c);
+	// Less than or equal to (<=)
+	printf("%d <= %d is %d \n", a, b, a <= b);
+	printf("%d <= %d is %d \n", a, c, a <= c);
 
-    return 0;
+	return 0;
 }
