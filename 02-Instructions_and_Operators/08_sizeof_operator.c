43ca22e06b35e522f5e14ae23e91c458b68ee2d3
DipadityaDas <DipadityaDas@gmail.com>
2023-05-11 23:51:32 +0530

    Revised sizeof operator

--- 02-Instructions_and_Operators/08_sizeof_operator.c
+++ 02-Instructions_and_Operators/08_sizeof_operator.c
@@ -7,19 +7,19 @@
 #include <stdio.h>
 int main()
 {
-    int a;
-    float b;
-    double c;
-    char d;
-    long e;
-    long double g;
+	int a;
+	float b;
+	double c;
+	char d;
+	long e;
+	long double g;
 
-    printf("Size of char    = %lu byte\n",  sizeof(d));
-    printf("Size of int     = %lu bytes\n", sizeof(a));
-    printf("Size of float   = %lu bytes\n", sizeof(b));
-    printf("Size of double  = %lu bytes\n", sizeof(c));
-    printf("Size of long    = %lu bytes\n", sizeof(e));
-    printf("Size of long double   = %lu bytes\n", sizeof(g));
+	printf("Size of char    = %lu byte\n",  sizeof(d));
+	printf("Size of int     = %lu bytes\n", sizeof(a));
+	printf("Size of float   = %lu bytes\n", sizeof(b));
+	printf("Size of double  = %lu bytes\n", sizeof(c));
+	printf("Size of long    = %lu bytes\n", sizeof(e));
+	printf("Size of long double   = %lu bytes\n", sizeof(g));
 
-    return 0;
+	return 0;
 }
