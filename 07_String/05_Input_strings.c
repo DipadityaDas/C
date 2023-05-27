72f2e0576322eb60c54419f075165d5ed69805b6
DipadityaDas <DipadityaDas@gmail.com>
2023-05-27 22:31:26 +0530

    Description of Input string

--- 07_String/05_Input_strings.c
+++ 07_String/05_Input_strings.c
@@ -1,3 +1,27 @@
+/*********************************************************************************************************************
+ * Input of a String in C
+ *
+ * Methods to Accept String With Space in C (Cont.)
+ *
+ * 2. fgets()
+ *
+ *    To overcome the above-listed limitation, we can use fgets() function. The fgets() function is similar to gets(),
+ * but we can also specify a maximum size for the string which will be taken as a string input in C from the user.
+ * This means that it reads either the complete line as a string and stores it in the array if the size is less than
+ * what is specified as a parameter (say: sz), else it reads only sz characters provided by the user so that no buffer
+ * overflow occurs.
+ *
+ * The syntax for fgets() in C is:
+ * char* fgets(char* s, int sz, FILE* stream)
+ *
+ * Here, s is the pointer which points to the array of characters where the input taken as a string will be stored
+ * and returned. The sz variable specifies the maximum allowed size for the user input and stream is used to denote
+ * the stream from which the input is to be taken (usually stdin, but it can also be a file).
+ *
+ *
+ *
+ */
+
 #include <stdio.h>
 
 int main()
